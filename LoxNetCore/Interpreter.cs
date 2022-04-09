a895c7a395e81fd6b99c2b5430c4e008b7efc8a1
Joshua Krohse <jkrohse@gmail.com>
2022-04-08 22:05:33 -0500

    Fixed mixed tabs and spaces

--- LoxNetCore/Interpreter.cs
+++ LoxNetCore/Interpreter.cs
@@ -3,162 +3,162 @@
 namespace LoxNetCore
 {
     public class Interpreter
-	{
-		private Env _environment = new Env();
-		private ErrorHandler _errorHandler = new ErrorHandler();
+    {
+        private Env _environment = new Env();
+        private ErrorHandler _errorHandler = new ErrorHandler();
 
-		public void Interpret(List<Stmt?> statements)
-		{
-			try
-			{
-				foreach (var statement in statements)
-				{
-					Execute(statement);
-				}
-			}
-			catch (RuntimeException error)
-			{
-				_errorHandler.RuntimeError(error);
-			}
-		}
-
-		private void Execute(Stmt? statement)
-		{
-			switch (statement)
-			{
-				case Stmt.Print printStmt:
-					HandlePrintStatement(printStmt);
-					break;
-				case Stmt.Var varStmt:
-					HandleVarStatement(varStmt);
-					break;
-				case Stmt.Expression expression:
-					HandleExpression(expression);
-					break;
-			}
-		}
-
-		private void HandlePrintStatement(Stmt.Print stmt) =>
-			Console.WriteLine(Stringify(Evaluate(stmt.Expr)));
-
-		private void HandleVarStatement(Stmt.Var stmt)
+        public void Interpret(List<Stmt?> statements)
         {
-			var value = (stmt.Initializer is null) ? null : Evaluate(stmt.Initializer);
-
-			_environment.Define(stmt.Name.Lexeme, value);
+            try
+            {
+                foreach (var statement in statements)
+                {
+                    Execute(statement);
+                }
+            }
+            catch (RuntimeException error)
+            {
+                _errorHandler.RuntimeError(error);
+            }
         }
 
-		public void HandleExpression(Stmt.Expression expression) => Evaluate(expression.Expr);
+        private void Execute(Stmt? statement)
+        {
+            switch (statement)
+            {
+                case Stmt.Print printStmt:
+                    HandlePrintStatement(printStmt);
+                    break;
+                case Stmt.Var varStmt:
+                    HandleVarStatement(varStmt);
+                    break;
+                case Stmt.Expression expression:
+                    HandleExpression(expression);
+                    break;
+            }
+        }
 
-		private object? Evaluate(Expr expression)
-		{
-			return expression switch
-			{
-				Expr.Literal literal => literal.Value,
-				Expr.Variable variable => RetrieveVariable(variable.Name),
-				Expr.Grouping grouping => Evaluate(grouping.Expr),
-				Expr.Unary unary => HandleUnary(unary),
-				Expr.Binary binary => HandleBinary(binary),
-				Expr.Ternary ternary => HandleTernary(ternary),
-				_ => throw new NotImplementedException()
-			};
-		}
+        private void HandlePrintStatement(Stmt.Print stmt) =>
+            Console.WriteLine(Stringify(Evaluate(stmt.Expr)));
 
-		private object? RetrieveVariable(Token name) => _environment.Get(name);
+        private void HandleVarStatement(Stmt.Var stmt)
+        {
+            var value = (stmt.Initializer is null) ? null : Evaluate(stmt.Initializer);
 
-		private object? HandleUnary(Expr.Unary unary)
-		{
-			var right = Evaluate(unary.Right);
+            _environment.Define(stmt.Name.Lexeme, value);
+        }
 
-			return unary.Op.Type switch
-			{
-				MINUS when right is double r => -r,
+        public void HandleExpression(Stmt.Expression expression) => Evaluate(expression.Expr);
 
-				BANG => !IsTruthy(right),
+        private object? Evaluate(Expr expression)
+        {
+            return expression switch
+            {
+                Expr.Literal literal => literal.Value,
+                Expr.Variable variable => RetrieveVariable(variable.Name),
+                Expr.Grouping grouping => Evaluate(grouping.Expr),
+                Expr.Unary unary => HandleUnary(unary),
+                Expr.Binary binary => HandleBinary(binary),
+                Expr.Ternary ternary => HandleTernary(ternary),
+                _ => throw new NotImplementedException()
+            };
+        }
 
-				_ => throw new RuntimeException(unary.Op, $"Unary operator {unary.Op.Lexeme} can't be applied to operand {right} with type of {right?.GetType().Name}"),
-			};
-		}
+        private object? RetrieveVariable(Token name) => _environment.Get(name);
 
-		private object? HandleTernary(Expr.Ternary ternary)
-		{
-			object? boolExpr = Evaluate(ternary.BoolExpr);
+        private object? HandleUnary(Expr.Unary unary)
+        {
+            var right = Evaluate(unary.Right);
 
-			return IsTruthy(boolExpr) ? Evaluate(ternary.TrueExpr) : Evaluate(ternary.FalseExpr);
-		}
+            return unary.Op.Type switch
+            {
+                MINUS when right is double r => -r,
 
-		private object? HandleBinary(Expr.Binary binary)
-		{
-			var left = Evaluate(binary.Left);
-			var right = Evaluate(binary.Right);
+                BANG => !IsTruthy(right),
 
-			return binary.Op.Type switch
-			{
-				MINUS when left is double l && right is double r => l - r,
+                _ => throw new RuntimeException(unary.Op, $"Unary operator {unary.Op.Lexeme} can't be applied to operand {right} with type of {right?.GetType().Name}"),
+            };
+        }
 
-				PLUS when left is double l && right is double r => l + r,
-				PLUS when left is string l && right is string r => l + r,
-				PLUS when left is double l && right is string r => l + r,
-				PLUS when left is string l && right is double r => l + r,
+        private object? HandleTernary(Expr.Ternary ternary)
+        {
+            object? boolExpr = Evaluate(ternary.BoolExpr);
 
-				SLASH when left is double l && right is double r && r == 0 => throw new RuntimeException(binary.Op, "Can't divide by 0"),
-				SLASH when left is double l && right is double r => l / r,
-				
-				STAR when left is double l && right is double r => l * r,
-				STAR when left is string l && right is double r => string.Concat(Enumerable.Repeat(l, (int) r)),
+            return IsTruthy(boolExpr) ? Evaluate(ternary.TrueExpr) : Evaluate(ternary.FalseExpr);
+        }
 
-				GREATER when left is double l && right is double r => l > r,
-				GREATER_EQUAL when left is double l && right is double r => l >= r,
+        private object? HandleBinary(Expr.Binary binary)
+        {
+            var left = Evaluate(binary.Left);
+            var right = Evaluate(binary.Right);
 
-				GREATER when left is string l && right is string r => l.CompareTo(r) > 0,
-				GREATER_EQUAL when left is string l && right is string r => l.CompareTo(r) >= 0,
+            return binary.Op.Type switch
+            {
+                MINUS when left is double l && right is double r => l - r,
 
-				LESS when left is double l && right is double r => l < r,
-				LESS_EQUAL when left is double l && right is double r => l <= r,
+                PLUS when left is double l && right is double r => l + r,
+                PLUS when left is string l && right is string r => l + r,
+                PLUS when left is double l && right is string r => l + r,
+                PLUS when left is string l && right is double r => l + r,
 
-				LESS when left is string l && right is string r => l.CompareTo(r) < 0,
-				LESS_EQUAL when left is string l && right is string r => l.CompareTo(r) <= 0,
+                SLASH when left is double l && right is double r && r == 0 => throw new RuntimeException(binary.Op, "Can't divide by 0"),
+                SLASH when left is double l && right is double r => l / r,
+                
+                STAR when left is double l && right is double r => l * r,
+                STAR when left is string l && right is double r => string.Concat(Enumerable.Repeat(l, (int) r)),
 
-				EQUAL_EQUAL => IsEqual(left, right),
-				BANG_EQUAL => !IsEqual(left, right),
+                GREATER when left is double l && right is double r => l > r,
+                GREATER_EQUAL when left is double l && right is double r => l >= r,
 
-				_ => throw new RuntimeException(binary.Op, $"Binary operator {binary.Op.Lexeme} can't be applied to left operand {Stringify(left)} ({left?.GetType().Name}) and right operand {Stringify(right)} ({right?.GetType().Name})"),
-			};
-		}
+                GREATER when left is string l && right is string r => l.CompareTo(r) > 0,
+                GREATER_EQUAL when left is string l && right is string r => l.CompareTo(r) >= 0,
 
-		private bool IsTruthy(object? obj)
-		{
-			if (obj is null) return false;
-			if (obj is bool) return (bool)obj;
-			return true;
-		}
+                LESS when left is double l && right is double r => l < r,
+                LESS_EQUAL when left is double l && right is double r => l <= r,
 
-		private bool IsEqual(object? a, object? b)
-		{
-			if (a is null && b is null) return true;
-			if (a is null) return false;
-			
-			return a.Equals(b);
-		}
+                LESS when left is string l && right is string r => l.CompareTo(r) < 0,
+                LESS_EQUAL when left is string l && right is string r => l.CompareTo(r) <= 0,
 
-		private string? Stringify(object? obj)
-		{
-			switch (obj)
-			{
-				case null:
-					return "nil";
-				case bool b:
-					return b ? "true" : "false";
-				case double d:
-					string text = d.ToString();
+                EQUAL_EQUAL => IsEqual(left, right),
+                BANG_EQUAL => !IsEqual(left, right),
 
-					if (text.EndsWith(".0"))
-						text = text[..-2];
+                _ => throw new RuntimeException(binary.Op, $"Binary operator {binary.Op.Lexeme} can't be applied to left operand {Stringify(left)} ({left?.GetType().Name}) and right operand {Stringify(right)} ({right?.GetType().Name})"),
+            };
+        }
 
-					return text;
-				default:
-					return obj.ToString();
-			}
-		}
-	}
+        private bool IsTruthy(object? obj)
+        {
+            if (obj is null) return false;
+            if (obj is bool) return (bool)obj;
+            return true;
+        }
+
+        private bool IsEqual(object? a, object? b)
+        {
+            if (a is null && b is null) return true;
+            if (a is null) return false;
+            
+            return a.Equals(b);
+        }
+
+        private string? Stringify(object? obj)
+        {
+            switch (obj)
+            {
+                case null:
+                    return "nil";
+                case bool b:
+                    return b ? "true" : "false";
+                case double d:
+                    string text = d.ToString();
+
+                    if (text.EndsWith(".0"))
+                        text = text[..-2];
+
+                    return text;
+                default:
+                    return obj.ToString();
+            }
+        }
+    }
 }
