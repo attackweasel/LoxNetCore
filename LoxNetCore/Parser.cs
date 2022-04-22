243325f7fb04fed8e635f2fb2c242ac4bde70e91
Joshua Krohse <jkrohse@gmail.com>
2022-04-22 17:41:15 -0500

    Put Synchronize method back the way it was supposed to be

--- LoxNetCore/Parser.cs
+++ LoxNetCore/Parser.cs
@@ -2,285 +2,284 @@
 
 namespace LoxNetCore
 {
-	public class Parser
-	{
-		private readonly List<Token> _tokens;
-		private int _current = 0;
-		private ErrorHandler _errorHandler;
+    public class Parser
+    {
+        private readonly List<Token> _tokens;
+        private int _current = 0;
+        private ErrorHandler _errorHandler;
 
-		public Parser(List<Token> tokens, ErrorHandler errorHandler)
-		{
-			_tokens = tokens;
-			_errorHandler = errorHandler;
-		}
-
-		public List<Stmt?> Parse()
-		{
-			List<Stmt?> statements = new List<Stmt?>();
-
-			while (!IsAtEnd())
-			{
-				statements.Add(Declaration());
-			}
-
-			return statements;
-		}
-
-		private Stmt? Declaration()
+        public Parser(List<Token> tokens, ErrorHandler errorHandler)
         {
-			try
+            _tokens = tokens;
+            _errorHandler = errorHandler;
+        }
+
+        public List<Stmt?> Parse()
+        {
+            List<Stmt?> statements = new List<Stmt?>();
+
+            while (!IsAtEnd())
             {
-				if (Match(VAR))
-					return VarDeclaration();
-				return Statement();
+                statements.Add(Declaration());
             }
-			catch (ParseException)
+
+            return statements;
+        }
+
+        private Stmt? Declaration()
+        {
+            try
             {
-				Synchronize();
-				return null;
+                if (Match(VAR))
+                    return VarDeclaration();
+                return Statement();
+            }
+            catch (ParseException)
+            {
+                Synchronize();
+                return null;
             }
         }
 
-		private Stmt VarDeclaration()
+        private Stmt VarDeclaration()
         {
-			Token name = Consume(IDENTIFIER, "Expect variable name.");
+            Token name = Consume(IDENTIFIER, "Expect variable name.");
 
-			Expr? initializer = Match(EQUAL) ? Expression() : null;
+            Expr? initializer = Match(EQUAL) ? Expression() : null;
 
-			Consume(SEMICOLON, "Expect ';' after variable declaration.");
+            Consume(SEMICOLON, "Expect ';' after variable declaration.");
 
-			return new Stmt.Var(name, initializer);
+            return new Stmt.Var(name, initializer);
         }
 
-		private Stmt Statement()
-		{
-			if (Match(PRINT)) return PrintStatement();
-			if (Match(LEFT_BRACE)) return new Stmt.Block(Block());
-			return ExpressionStatement();
-		}
-
-		private Stmt PrintStatement()
-		{
-			Expr value = Expression();
-			Consume(SEMICOLON, "Expect ';' after value.");
-			return new Stmt.Print(value);
-		}
-
-		private List<Stmt?> Block()
+        private Stmt Statement()
         {
-			List<Stmt?> statements = new List<Stmt?>();
-
-			while (!Check(RIGHT_BRACE) && !IsAtEnd())
-				statements.Add(Declaration());
-
-			Consume(RIGHT_BRACE, "Expect '}' after block.");
-
-			return statements;
+            if (Match(PRINT)) return PrintStatement();
+            if (Match(LEFT_BRACE)) return new Stmt.Block(Block());
+            return ExpressionStatement();
         }
 
-		private Stmt ExpressionStatement()
-		{
-			Expr value = Expression();
-			Consume(SEMICOLON, "Expect ';' after value.");
-			return new Stmt.Expression(value);
-		}
-
-		private Expr Expression() => Assignment();
-
-		private Expr Assignment()
+        private Stmt PrintStatement()
         {
-			Expr expr = Ternary();
+            Expr value = Expression();
+            Consume(SEMICOLON, "Expect ';' after value.");
+            return new Stmt.Print(value);
+        }
 
-			if (Match(EQUAL))
+        private List<Stmt?> Block()
+        {
+            List<Stmt?> statements = new List<Stmt?>();
+
+            while (!Check(RIGHT_BRACE) && !IsAtEnd())
+                statements.Add(Declaration());
+
+            Consume(RIGHT_BRACE, "Expect '}' after block.");
+
+            return statements;
+        }
+
+        private Stmt ExpressionStatement()
+        {
+            Expr value = Expression();
+            Consume(SEMICOLON, "Expect ';' after value.");
+            return new Stmt.Expression(value);
+        }
+
+        private Expr Expression() => Assignment();
+
+        private Expr Assignment()
+        {
+            Expr expr = Ternary();
+
+            if (Match(EQUAL))
             {
-				Token equals = Previous();
-				Expr value = Assignment();
+                Token equals = Previous();
+                Expr value = Assignment();
 
-				if (expr is Expr.Variable variable)
+                if (expr is Expr.Variable variable)
                 {
-					Token name = variable.Name;
-					return new Expr.Assign(name, value);
+                    Token name = variable.Name;
+                    return new Expr.Assign(name, value);
                 }
 
-				_errorHandler.Error(equals, "Invalid assignment target.");
+                _errorHandler.Error(equals, "Invalid assignment target.");
             }
 
-			return expr;
+            return expr;
         }
 
-		private Expr Ternary()
-		{
-			Expr expr = Equality();
+        private Expr Ternary()
+        {
+            Expr expr = Equality();
 
-			if (Match(QUESTION))
-			{
-				Token op = new Token(TERNARY, "?:", null, Previous().Line);
+            if (Match(QUESTION))
+            {
+                Token op = new Token(TERNARY, "?:", null, Previous().Line);
 
-				Expr trueExpr = Equality();
+                Expr trueExpr = Equality();
 
-				if (Match(COLON))
-				{
-					Expr falseExpr = Equality();
-					expr = new Expr.Ternary(expr, op, trueExpr, falseExpr);
-				}
-				else
-				{
-					throw Error(new Token(TERNARY, "?:", null, Previous().Line), "Ternary expression requires ':'");
-				}
-			}
+                if (Match(COLON))
+                {
+                    Expr falseExpr = Equality();
+                    expr = new Expr.Ternary(expr, op, trueExpr, falseExpr);
+                }
+                else
+                {
+                    throw Error(new Token(TERNARY, "?:", null, Previous().Line), "Ternary expression requires ':'");
+                }
+            }
 
-			return expr;
-		}
+            return expr;
+        }
 
-		private Expr Equality()
-		{
-			Expr expr = Comparison();
+        private Expr Equality()
+        {
+            Expr expr = Comparison();
 
-			while (Match(BANG_EQUAL, EQUAL_EQUAL))
-			{
-				Token op = Previous();
-				Expr right = Comparison();
-				expr = new Expr.Binary(expr, op, right);
-			}
+            while (Match(BANG_EQUAL, EQUAL_EQUAL))
+            {
+                Token op = Previous();
+                Expr right = Comparison();
+                expr = new Expr.Binary(expr, op, right);
+            }
 
-			return expr;
-		}
+            return expr;
+        }
 
-		private Expr Comparison()
-		{
-			Expr expr = Term();
+        private Expr Comparison()
+        {
+            Expr expr = Term();
 
-			while (Match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL))
-			{
-				Token op = Previous();
-				Expr right = Term();
-				expr = new Expr.Binary(expr, op, right);
-			}
+            while (Match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL))
+            {
+                Token op = Previous();
+                Expr right = Term();
+                expr = new Expr.Binary(expr, op, right);
+            }
 
-			return expr;
-		}
+            return expr;
+        }
 
-		private Expr Term()
-		{
-			Expr expr = Factor();
+        private Expr Term()
+        {
+            Expr expr = Factor();
 
-			while (Match(MINUS, PLUS))
-			{
-				Token op = Previous();
-				Expr right = Factor();
-				expr = new Expr.Binary(expr, op, right);
-			}
+            while (Match(MINUS, PLUS))
+            {
+                Token op = Previous();
+                Expr right = Factor();
+                expr = new Expr.Binary(expr, op, right);
+            }
 
-			return expr;
-		}
+            return expr;
+        }
 
-		private Expr Factor()
-		{
-			Expr expr = Unary();
+        private Expr Factor()
+        {
+            Expr expr = Unary();
 
-			while (Match(SLASH, STAR))
-			{
-				Token op = Previous();
-				Expr right = Unary();
-				expr = new Expr.Binary(expr, op, right);
-			}
+            while (Match(SLASH, STAR))
+            {
+                Token op = Previous();
+                Expr right = Unary();
+                expr = new Expr.Binary(expr, op, right);
+            }
 
-			return expr;
-		}
+            return expr;
+        }
 
-		private Expr Unary()
-		{
-			if (Match(BANG, MINUS))
-			{
-				Token op = Previous();
-				Expr right = Unary();
-				return new Expr.Unary(op, right);
-			}
+        private Expr Unary()
+        {
+            if (Match(BANG, MINUS))
+            {
+                Token op = Previous();
+                Expr right = Unary();
+                return new Expr.Unary(op, right);
+            }
 
-			return Primary();
-		}
+            return Primary();
+        }
 
-		private Expr Primary()
-		{
-			if (Match(FALSE)) return new Expr.Literal(false);
-			if (Match(TRUE)) return new Expr.Literal(true);
-			if (Match(NIL)) return new Expr.Literal(null);
+        private Expr Primary()
+        {
+            if (Match(FALSE)) return new Expr.Literal(false);
+            if (Match(TRUE)) return new Expr.Literal(true);
+            if (Match(NIL)) return new Expr.Literal(null);
 
-			if (Match(NUMBER, STRING)) return new Expr.Literal(Previous().Literal);
+            if (Match(NUMBER, STRING)) return new Expr.Literal(Previous().Literal);
 
-			if (Match(IDENTIFIER)) return new Expr.Variable(Previous());
+            if (Match(IDENTIFIER)) return new Expr.Variable(Previous());
 
-			if (Match(LEFT_PAREN))
-			{
-				Expr expr = Expression();
-				Consume(RIGHT_PAREN, "Expect ')' after expression.");
-				return new Expr.Grouping(expr);
-			}
+            if (Match(LEFT_PAREN))
+            {
+                Expr expr = Expression();
+                Consume(RIGHT_PAREN, "Expect ')' after expression.");
+                return new Expr.Grouping(expr);
+            }
 
-			throw Error(Peek(), "Expect expression.");
-		}
+            throw Error(Peek(), "Expect expression.");
+        }
 
-		private bool Match(params TokenType[] types)
-		{
-			foreach(TokenType type in types)
-			{
-				if (Check(type))
-				{
-					Advance();
-					return true;
-				}
-			}
-			return false;
-		}
+        private bool Match(params TokenType[] types)
+        {
+            foreach(TokenType type in types)
+            {
+                if (Check(type))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+            return false;
+        }
 
-		private Token Consume(TokenType type, string message)
-		{
-			if (Check(type)) return Advance();
+        private Token Consume(TokenType type, string message)
+        {
+            if (Check(type)) return Advance();
 
-			throw Error(Peek(), message);
-		}
+            throw Error(Peek(), message);
+        }
 
-		private bool Check(TokenType type)
-		{
-			if (IsAtEnd()) return false;
+        private bool Check(TokenType type)
+        {
+            if (IsAtEnd()) return false;
 
-			return Peek().Type == type;
-		}
+            return Peek().Type == type;
+        }
 
-		private Token Advance()
-		{
-			if (!IsAtEnd()) _current++;
+        private Token Advance()
+        {
+            if (!IsAtEnd()) _current++;
 
-			return Previous();
-		}
+            return Previous();
+        }
 
-		private bool IsAtEnd() => Peek().Type == EOF;
-		
-		private Token Peek() => _tokens[_current];
+        private bool IsAtEnd() => Peek().Type == EOF;
+        
+        private Token Peek() => _tokens[_current];
 
-		private Token Previous() => _tokens[_current - 1];
+        private Token Previous() => _tokens[_current - 1];
 
-		private ParseException Error(Token token, string message)
-		{
-			_errorHandler.Error(token, message);
-			return new ParseException();
-		}
+        private ParseException Error(Token token, string message)
+        {
+            _errorHandler.Error(token, message);
+            return new ParseException();
+        }
 
-		private void Synchronize()
-		{
-			while(!IsAtEnd())
-			{
-				Advance();
-				if (Previous().Type == SEMICOLON) return;
-			}
+        private void Synchronize()
+        {
+            Advance();
 
-			switch (Peek().Type)
-			{
-				case CLASS or FOR or FUN or IF or PRINT or RETURN or VAR or WHILE:
-					return;
-			}
+            while(!IsAtEnd())
+            {
+                if (Previous().Type is SEMICOLON) return;
 
-			Advance();
-		}
-	}
+                if (Peek().Type is CLASS or FOR or FUN or IF or PRINT
+                                or RETURN or VAR or WHILE)
+                    return;
+
+                Advance();
+            }
+        }
+    }
 }
